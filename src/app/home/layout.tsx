a60a389e2e5c88c0ef0bc2050b232e889a0f0ac3
renz <dev.renzsalanga@gmail.com>
2025-05-24 19:35:37 +0800

    refactor: home layout mobile responsiveness (wip)

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -5,33 +5,40 @@ import NotificationIcon from '@/assets/bell.svg'
 import HomeIcon from '@/assets/home.svg'
 import StarIcon from '@/assets/star-off.svg'
 import FolderIcon from '@/assets/folder.svg'
+import PlusIcon from '@/assets/plus.svg'
+import Search from '@/app/home/search'
+import MoreIcon from '@/assets/more.svg'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="grid grid-cols-[300px_1fr] grid-rows-[56px_1fr] h-full">
             <div className="col-span-2 border-b border-surfaceVariant flex">
-                <div className="flex items-center h-full">
+                <div className="flex items-center h-full ms-4">
                     <Image src="/brand.png" alt="threads-web" height={200} width={200} className="size-[34px] inline-block" />
                     <span className="md-sys-typescale-title-large ms-2">Threads</span>
                 </div>
-                <div className="grow-1 flex items-center justify-center px-12">
-                    <input placeholder="Search" type="text" name="search" id="search" className="md-sys-typescale-body-medium h-[35px] rounded-full border-surfaceVariant bg-surfaceContainer focus:bg-surfaceContainerHighest w-full max-w-[500px] outline-primary px-4" />
+                <div className="grow-1 flex items-center justify-center h-full">
+                    <Search />
                 </div>
-                <div className="flex items-center justify-center gap-2">
-                    <a className="inline-block group rounded-full border border-surfaceVariant size-[35px] p-[4px] hover:bg-primary select-none">
+                <div className="flex items-center justify-center gap-2 me-4">
+                    <a className="hidden lg:inline-block group rounded-full size-[35px] p-[4px] hover:bg-primary select-none">
 						<MessageIcon className="size-full group-hover:text-onPrimary" />
 					</a>
 
-                    <a className="inline-block group rounded-full border border-surfaceVariant size-[35px] p-[4px] hover:bg-primary select-none">
+                    <a className="hidden lg:inline-block group rounded-full size-[35px] p-[4px] hover:bg-primary select-none">
 						<NotificationIcon className="size-full group-hover:text-onPrimary" />
 					</a>
 					
-					<a className="inline-block rounded-full border border-surfaceVariant size-[35px] p-[2px] hover:bg-primary select-none">
+					<a className="hidden lg:inline-block rounded-full size-[35px] p-[2px] hover:bg-primary select-none">
 						<Image src="https://avatar.iran.liara.run/public/boy" alt="Avatar" height={500} width={500} className="size-full" />
+					</a>
+
+                    <a className="inline-block lg:hidden group rounded-full size-[35px] p-[4px] hover:bg-primary select-none">
+						<MoreIcon className="size-full group-hover:text-onPrimary" />
 					</a>
                 </div>
             </div>
-            <nav className="border-r border-surfaceVariant overflow-auto">
+            <nav className="hidden lg:block border-r border-surfaceVariant overflow-auto">
                 <ul className="m-4">
                     <li>
                         <a className="group flex items-center px-4 py-3 rounded-lg hover:bg-primary">
@@ -60,6 +67,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
                 <ul className="m-4">
                     <li>
+                        <a className="group flex items-center px-4 py-3 rounded-lg hover:bg-primary">
+                            <PlusIcon className="inline-block size-[20px] text-tertiary group-hover:text-onPrimary" />
+                            <span className="md-sys-typescale-label-large ms-2 text-tertiary group-hover:text-onPrimary">Create a custom feed</span>
+                        </a>
+                    </li>
+
+                    <li className="mt-2">
                         <a className="group flex items-center px-4 py-3 rounded-lg hover:bg-primary">
                             <Image src="https://avatar.iran.liara.run/public/boy" alt="Avatar" height={500} width={500} className="inline-block size-[20px]" />
                             <span className="md-sys-typescale-label-large ms-2 group-hover:text-onPrimary">r/Android</span>
@@ -93,6 +107,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
                 <ul className="m-4">
                     <li>
+                        <a className="group flex items-center px-4 py-3 rounded-lg hover:bg-primary">
+                            <PlusIcon className="inline-block size-[20px] text-tertiary group-hover:text-onPrimary" />
+                            <span className="md-sys-typescale-label-large ms-2 text-tertiary group-hover:text-onPrimary">Create a community</span>
+                        </a>
+                    </li>
+
+                    <li className="mt-2">
                         <a className="group flex items-center px-4 py-3 rounded-lg hover:bg-primary">
                             <Image src="https://avatar.iran.liara.run/public/boy" alt="Avatar" height={500} width={500} className="inline-block size-[20px]" />
                             <span className="md-sys-typescale-label-large ms-2 group-hover:text-onPrimary">r/Android</span>
---
+++ src/app/home/search.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import SearchIcon from '@/assets/search.svg'
+import clsx from 'clsx'
+import { useCallback, useState } from 'react'
+
+export default function Search() {
+    const [isFocused, setIsFocused] = useState(false)
+    const handleFocus = useCallback(() => { setIsFocused(true) }, [])
+    const handleBlur = useCallback(() => { setIsFocused(false) }, [])
+
+    return (
+        <div 
+            className={clsx(
+                "hidden lg:flex border rounded-full border-surfaceVariant w-full max-w-[500px] items-center px-4",
+                {
+                    "outline-primary outline-2": isFocused,
+                    "bg-surfaceContainer": !isFocused,
+                    "bg-surfaceContainerHighest": isFocused
+                }
+            )}
+        >
+            <SearchIcon className="inline-block size-[20px]" />
+            <input 
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                placeholder="Search" 
+                type="text" 
+                name="search" 
+                id="search"
+                className="md-sys-typescale-body-large h-[34px] outline-none grow ms-4"
+            />
+        </div>
+    )
+}
