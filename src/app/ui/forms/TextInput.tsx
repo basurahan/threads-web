fb7c71ad08a073a65dc65612ad847c0edf3c0c56
renz <dev.renzsalanga@gmail.com>
2025-05-26 09:23:54 +0800

    refactor: made textinput component more readable and flexible

--- src/app/ui/forms/TextInput.tsx
+++ src/app/ui/forms/TextInput.tsx
@@ -8,9 +8,9 @@ import Toggle from '@/app/ui/forms/Toggle'
 type TextInput = "text" | "password"
 
 interface ITextInput {
-    type: TextInput, 
-    name: string, 
-    label: string, 
+    type: TextInput,
+    name: string,
+    label: string,
     isRequired?: boolean,
     defaultValue?: string,
     error?: string,
@@ -18,15 +18,15 @@ interface ITextInput {
 }
 
 export default function TextInput(
-    { 
-        type, 
-        name, 
+    {
+        type,
+        name,
         label,
         isRequired,
-        defaultValue, 
+        defaultValue,
         error,
-        className 
-    } : ITextInput
+        className
+    }: ITextInput
 ) {
     const [isPasswordShown, setIsPasswordShown] = useState(false)
     const [isFocused, setIsFocused] = useState(false)
@@ -35,41 +35,50 @@ export default function TextInput(
     const handleFocus = useCallback(() => { setIsFocused(true) }, [])
     const handleBlur = useCallback(() => { setIsFocused(false) }, [])
 
+    const labelStyle = clsx(
+        "block md-sys-typescale-label-large mb-3",
+        {
+            "text-onBackground": !error,
+            "text-error": error
+        }
+    )
+
+    const inputContainerStyle = clsx(
+        "flex flex-row items-center h-[45px] rounded-md px-2",
+        {
+            "outline-surfaceVariant outline-2": !isFocused && !error,
+            "outline-error outline-4": !isFocused && error,
+            "outline-primary outline-4": isFocused
+        }
+    )
+
+    const inputType: TextInput = (isPasswordShown || type === "text") ? "text" : "password"
+
     return (
         <div className={className}>
-            <label 
-                htmlFor={name} 
-                className={clsx(
-                    "block md-sys-typescale-label-large mb-3",
-                    {
-                        "text-onBackground": !error,
-                        "text-error": error
-                    }
-                )}
-            >
+            <label htmlFor={name} className={labelStyle}>
                 {label}
             </label>
-            <div 
-                className={
-                    clsx(
-                        "flex flex-row items-center h-[45px] rounded-md px-2",
-                        {
-                            "outline-surfaceVariant outline-2": !isFocused && !error,
-                            "outline-error outline-4": !isFocused && error,
-                            "outline-primary outline-4": isFocused
-                        }
-                    )
-                }
-            >
+            <div className={inputContainerStyle}>
                 <input
-                    type={(isPasswordShown || type === "text") ? "text" : "password"}
-                    name={name} 
-                    id={name} 
+                    type={inputType}
+                    name={name}
+                    id={name}
                     defaultValue={defaultValue}
                     required={isRequired}
-                    className="h-full outline-none grow md-sys-typescale-body-large" onFocus={handleFocus} onBlur={handleBlur}
+                    className="h-full outline-none grow md-sys-typescale-body-large" 
+                    onFocus={handleFocus} 
+                    onBlur={handleBlur}
                 />
-                { type === "password" && <Toggle className="size-[34px] ms-3" onClick={handleToggleClick} initial={<EyeSlashIcon className="size-full text-secondary" />} checked={<EyeIcon className="size-full text-secondary" />}  /> }
+                {
+                    type === "password" && 
+                    <Toggle 
+                        className="size-[34px] ms-3" 
+                        onClick={handleToggleClick} 
+                        initial={<EyeSlashIcon className="size-full text-secondary" />} 
+                        checked={<EyeIcon className="size-full text-secondary" />} 
+                    />
+                }
             </div>
             <p className="md-sys-typescale-label-small min-h-[var(--md-sys-typescale-label-small-line-height)] mt-2 ms-1 text-error">{error}</p>
         </div>
