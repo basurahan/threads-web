967a9ca84cb448b2577408f85a1a9655e6c2a6cc
renz <dev.renzsalanga@gmail.com>
2025-05-26 09:37:56 +0800

    refactor: remove test routes

--- src/app/token/route.ts
+++
@@ -1,7 +0,0 @@
-import { generateToken } from '@/app/util/jwt'
-
-export async function GET() {
-    //const { searchParams } = new URL(request.nextUrl)
-    const token = await generateToken({ sub: "123" })
-    return Response.json({ hello: token })
-}
--- src/app/token/verify/route.ts
+++
@@ -1,8 +0,0 @@
-import { verifyToken } from '@/app/util/jwt'
-import { NextRequest } from 'next/server'
-
-export async function GET(request: NextRequest) {
-    const { searchParams } = new URL(request.nextUrl)
-    const item = await verifyToken(searchParams.get("token") as string)
-    return Response.json({ test: item })
-}
