0bc34bd9b12ae51e059b22ce31728d941beba485
renz <dev.renzsalanga@gmail.com>
2025-05-26 09:31:23 +0800

    refactor: spinner component to be more readable and flexible

--- src/app/ui/forms/Spinner.tsx
+++ src/app/ui/forms/Spinner.tsx
@@ -1,19 +1,14 @@
 import styles from '@/app/ui/forms/styles/spinner.module.css'
 import clsx from 'clsx'
 
-export default function Spinner({ size, className } : { size: number, className?: string }) {
+export default function Spinner({ className }: { className?: string }) {
+
+    const containerStyle = clsx(
+        styles.loader,
+        className
+    )
+
     return (
-        <span 
-            className={
-                clsx(
-                    styles.loader,
-                    className
-                )
-            }
-            style={{
-                height: size,
-                width: size
-            }}
-        />
+        <span className={containerStyle} />
     )
 }
--- src/app/ui/forms/Submit.tsx
+++ src/app/ui/forms/Submit.tsx
@@ -4,18 +4,23 @@ import { useFormStatus } from 'react-dom'
 import Spinner from '@/app/ui/forms/Spinner'
 import clsx from 'clsx'
 
-export default function Submit({ label, className } : { label: string, className?: string }) {
+export default function Submit({ label, className }: { label: string, className?: string }) {
     const status = useFormStatus()
 
+    const containerStyle = clsx(
+        className,
+        "relative"
+    )
+
     return (
-        <div 
-            className={clsx(
-                className,
-                "relative"
-            )}
-        >
-            <button disabled={status.pending} type="submit" className="h-[45px] w-full bg-primary rounded-md text-onPrimary hover:bg-onPrimaryContainer shadow-md md-sys-typescale-label-large disabled:bg-surfaceDim">{label}</button>
-            { status.pending && <Spinner size={24} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-onSurface border-3" /> }
+        <div className={containerStyle}>
+            <button
+                disabled={status.pending}
+                type="submit"
+                className="h-[45px] w-full bg-primary rounded-md text-onPrimary hover:bg-onPrimaryContainer shadow-md md-sys-typescale-label-large disabled:bg-surfaceDim">
+                {label}
+            </button>
+            {status.pending && <Spinner className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-onSurface border-3 size-6" />}
         </div>
     )
 }
